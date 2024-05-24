c63feb416cdc4b0eee8be12bf569007b126dbee3
JOAO PINHEIRO <42075957835@info.local>
2024-05-24 10:56:12 -0300

    feat: Começo de JSON, partes dos cards feito.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function abreFechaMenu() {
 
 onresize = () => {
     menu.classList.remove("menu-fechado")
-     barras.style.display = "none"
+    barras.style.display = "none"
     ex.style.display = "inline"
 }
 
@@ -38,22 +38,55 @@ function nextSlide() {
     else {
         slideAtual = 0
     }
-    
+
 }
 function previousSlide() {
     banner.classList.remove(slides[slideAtual])
     if (slideAtual > 0) {
-    slideAtual--
-    banner.classList.add(slides[slideAtual])
+        slideAtual--
+        banner.classList.add(slides[slideAtual])
     } else {
         slideAtual = 2
     }
-    
-    
+
+
 }
 
-function selectSlide(indiceSlide){
+function selectSlide(indiceSlide) {
     banner.classList.remove(slides[slideAtual])
     slideAtual = indiceSlide
     banner.classList.add(slides[slideAtual])
+}
+
+// let listaCases = [
+    // {
+    //     imagem: "https://unsplash.it/600/400?image=43",
+    //     descricao: "Uma empresa de tecnologia lança um desafio de gamificação onde os funcionarios devem propor e implementar ideias inovadoras.",
+    // },
+    // {
+    //     imagem: "https://unsplash.it/600/400?image=65",
+    //     descricao: "Uma empresa de consultoria cria uma narrativa interativa de gamificação para seu programa de treinamento",
+    // },
+    // {
+    //     imagem: "https://unsplash.it/600/400?image=22",
+    //     descricao: "Uma empresa de vendas implementa uma competição gamificada entre equipes que competem pelo topo do ranking",
+    // },
+    // {
+    //     imagem: "https://unsplash.it/600/400?image=94",
+    //     descricao: "Uma empresa de saúde promove o bem-estar dos funcionários através de um desafio de gamificação de condicionamento físico",
+    // },
+// ]
+
+function renderCases() {
+    let containerCards = document.querySelector(".container-cards")
+    let template = ""
+    listaCases.forEach(cardCase => {
+        template += `<div class="card">
+       <img src=${ cardCase.imagem } alt="">
+       <p>${ cardCase.descricao }</p>
+       <button>Ver Mais</button>
+   </div>`
+    })
+
+    containerCards.innerHTML = template
 }
